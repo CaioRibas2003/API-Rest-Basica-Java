ce8dfdfb38d3266241891d7ceb527332ea484d86
Caio Ribas <caioribasemm@gmail.com>
2025-11-04 19:26:28 -0300

    Request Mapping e ResponseEntity: refactor

--- src/main/java/br/com/caio/api_rest_basico/controllers/UsuarioController.java
+++ src/main/java/br/com/caio/api_rest_basico/controllers/UsuarioController.java
@@ -1,28 +1,27 @@
 package br.com.caio.api_rest_basico.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RequestMapping("usuarios")
 @RestController
 public class UsuarioController {
 
     private int id;
     private List<Usuario> user = new ArrayList<>();
 
-    @PostMapping("/usuarios/novo")
-    public boolean NovoUsuario(@RequestBody Usuario usuario) {
+    @PostMapping("/novo")
+    public ResponseEntity<Boolean> NovoUsuario(@RequestBody Usuario usuario) {
 
         usuario.setId(++id);
         user.add(usuario);
-        return true;
+        return ResponseEntity.status(201).body(true);
     }
 
-    @GetMapping("/usuarios/lista")
+    @GetMapping("/lista")
     public List<Usuario> getUser() {
         return user;
     }
