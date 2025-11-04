efcd8459c6a167687d8389f4281c77f4a64d8675
Caio Ribas <caioribasemm@gmail.com>
2025-11-04 20:03:03 -0300

    Editar request

--- src/main/java/br/com/caio/api_rest_basico/controllers/Usuario.java
+++ src/main/java/br/com/caio/api_rest_basico/controllers/Usuario.java
@@ -44,4 +44,5 @@ public class Usuario {
     public void setSenha(String senha) {
         this.senha = senha;
     }
+
 }
--- src/main/java/br/com/caio/api_rest_basico/controllers/UsuarioController.java
+++ src/main/java/br/com/caio/api_rest_basico/controllers/UsuarioController.java
@@ -11,18 +11,42 @@ import java.util.List;
 public class UsuarioController {
 
     private int id;
-    private List<Usuario> user = new ArrayList<>();
+    private List<Usuario> users = new ArrayList<>();
 
     @PostMapping("/novo")
     public ResponseEntity<Boolean> NovoUsuario(@RequestBody Usuario usuario) {
 
         usuario.setId(++id);
-        user.add(usuario);
+        users.add(usuario);
         return ResponseEntity.status(201).body(true);
     }
 
     @GetMapping("/lista")
-    public List<Usuario> getUser() {
-        return user;
+    public List<Usuario> getUsers() {
+        return users;
+    }
+
+    @PutMapping("/editar")
+    public ResponseEntity<Boolean> editarUsuario(@RequestBody Usuario usuario) {
+
+        boolean atualizarUsuario = false;
+        int index = 0;
+
+        for (Usuario usuarioAux: users) {
+
+            if (!atualizarUsuario) {
+                index++;
+            }
+            if (usuarioAux.getId() == usuario.getId()) {
+                atualizarUsuario = true;
+            }
+        }
+
+        if (atualizarUsuario) {
+            users.set(index - 1, usuario);
+            return ResponseEntity.ok(true);
+        } else {
+            return ResponseEntity.badRequest().body(false);
+        }
     }
 }
